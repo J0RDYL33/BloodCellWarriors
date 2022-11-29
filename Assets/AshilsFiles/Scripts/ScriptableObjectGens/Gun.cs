43cafc20907f85639de426ce340cb81cf5d15bc3
Ashil Sabu <ashilsabu7@gmail.com>
2022-11-29 17:50:15 +0000

    Weapon aiming and Kickback added, recoil commented out as I need to find a way to reset the weapon position after recoil.

--- Assets/AshilsFiles/Scripts/ScriptableObjectGens/Gun.cs
+++ Assets/AshilsFiles/Scripts/ScriptableObjectGens/Gun.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 public class Gun : ScriptableObject
 {
     public string name;
-    public float firerate;
+    public float fireRate;
+    public float aimSpeed;
+    public float bloom;
+    public float recoil;
+    public float kickBack;
+
     public GameObject prefab;
 }
--- Assets/AshilsFiles/Scripts/WeaponPistol.cs
+++ Assets/AshilsFiles/Scripts/WeaponPistol.cs
@@ -6,6 +6,11 @@ public class WeaponPistol : MonoBehaviour
 {
     public Gun[] loadout;
     public Transform weaponParent;
+
+    public GameObject bulletholePrefab;
+    public LayerMask canBeShot;
+
+    private int currentIndex;
     private GameObject currentWeapon;
 
     // Start is called before the first frame update
@@ -18,6 +23,15 @@ public class WeaponPistol : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1)) Equip(0);
+        if (currentWeapon != null)
+        {
+            Aim(Input.GetMouseButton(1));
+            if (Input.GetMouseButton(0))
+            {
+                Shoot();
+            }
+            currentWeapon.transform.localPosition = Vector3.Lerp(currentWeapon.transform.localPosition, Vector3.zero, Time.deltaTime * 4f);
+        }
     }
 
     // Chooses the weapon to be Equipped from the loadout
@@ -25,10 +39,67 @@ public class WeaponPistol : MonoBehaviour
     {
         if (currentWeapon != null) Destroy(currentWeapon);
 
+        currentIndex = p_ind;
+
         GameObject t_newWeapon = Instantiate(loadout[p_ind].prefab, weaponParent.position, weaponParent.rotation, weaponParent) as GameObject;
         t_newWeapon.transform.localPosition = Vector3.zero;
         t_newWeapon.transform.localEulerAngles = Vector3.zero;
 
         currentWeapon = t_newWeapon;
+    }
+
+    void Aim(bool p_isAiming)
+    {
+        Transform t_anchor = currentWeapon.transform.Find("Anchor");
+        Transform t_state_ads = currentWeapon.transform.Find("States/ADS");
+        Transform t_state_hip = currentWeapon.transform.Find("States/Hip");
+
+        if (p_isAiming)
+        {
+            //is Aiming
+            t_anchor.position = Vector3.Lerp(t_anchor.position, t_state_ads.position, Time.deltaTime * loadout[currentIndex].aimSpeed);
+        }
+        else
+        {
+            //not Aiming
+            t_anchor.position = Vector3.Lerp(t_anchor.position, t_state_hip.position, Time.deltaTime * loadout[currentIndex].aimSpeed);
+        }
+    }
+     
+    void Shoot()
+    {
+        Transform t_spawn = transform.Find("PlayerCameraRoot");
+
+        /*//Weapon Bloom
+        Vector3 t_bloom = t_spawn.position + t_spawn.forward * 1000f;
+        t_bloom += Random.Range(-loadout[currentIndex].bloom, loadout[currentIndex].bloom) * t_spawn.up;
+        t_bloom += Random.Range(-loadout[currentIndex].bloom, loadout[currentIndex].bloom) * t_spawn.right;
+        t_bloom -= t_spawn.position;
+        t_bloom.Normalize();
+
+        //Raycast
+        RaycastHit t_hit = new RaycastHit();
+        if (Physics.Raycast(t_spawn.position, t_bloom, out t_hit, 1000f, canBeShot))
+        {
+            GameObject t_newHole = Instantiate(bulletholePrefab, t_hit.point + t_hit.normal * 0.001f, Quaternion.identity) as GameObject;
+            t_newHole.transform.LookAt(t_hit.point + t_hit.normal);
+            Destroy(t_newHole, 5f);
+        }*/
+
+
+        // Quaternion initial_rotation = currentWeapon.transform.localRotation;
+
+        //WeaponRecoil
+        //currentWeapon.transform.Rotate(-loadout[currentIndex].recoil, 0, 0);
+
+        //Weapon Kickback
+        currentWeapon.transform.position -= currentWeapon.transform.forward * loadout[currentIndex].kickBack;
+
+        //Quaternion final_rotation = currentWeapon.transform.localRotation;
+
+        //currentWeapon.transform.localRotation = Quaternion.Lerp(final_rotation, initial_rotation, 1f);
+
+
+
     }
 }
